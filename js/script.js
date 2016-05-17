9f32725743d7d638c8ddc2154afd3ae07278a87f
Marcus Warnsley <mwarnsley@Marcuss-MacBook-Pro.local>
2016-05-16 21:25:12 -0400

    Editing the rest of the static content and structuring the controllers and routes for the pages

--- js/script.js
+++ js/script.js
@@ -8,9 +8,29 @@ var app = angular.module("computer", ['ngRoute'])
             templateUrl: '../pages/main.html',
             controller: 'MainCtrl'
     })
+        .when('/about', {
+                templateUrl: '../pages/about.html',
+                controller: 'MainCtrl'
+        })
+        .when('/services', {
+                templateUrl: '../pages/services.html',
+                controller: 'ServicesCtrl'
+        })
+        .when('/contact', {
+                templateUrl: '../pages/contact.html',
+                controller: 'ContactCtrl'
+        })
         .otherwise({redirectTo: '/main'});
 }])
 
 .controller('MainCtrl', ['$scope', function($scope){
-    console.log($scope);
-}]);
+
+}])
+
+.controller('ServicesCtrl', ['$scope', function($scope){
+    
+}])
+
+.controller('ContactCtrl', ['$scope', function($scope){
+
+}])
